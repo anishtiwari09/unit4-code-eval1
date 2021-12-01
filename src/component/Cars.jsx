8243ecd2f62b773ed003f6d81e43e29b84a2f3f5
anishtiwari09 <aneeshtiwari24@gmail.com>
2021-12-01 21:02:01 +0530

    filter work is complet

--- src/component/Cars.jsx
+++ src/component/Cars.jsx
@@ -2,23 +2,35 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import CarsDetail from "./carItem"
 export default function Cars(){
+    const [filter,setFilter]=useState(null)
     const [datas,setDatas]=useState([])
     const [isLoading,setIsLoading ]=useState(true)
-    const getCarData=()=>{
-        const config={
+    const getCarData=(filter)=>{
+        console.log('filter',filter)
+        if(!filter){
+        var config={
             url:"http://localhost:3000/cars",
             method:"get"
-        }
+        }}
+        else
+        {
+        var config={
+            url:`http://localhost:3000/cars?_sort=${filter}&_order=asc`,
+            method:"get"
+        }}
         return axios(config)
     }
     useEffect(()=>{
-        handleGetData()
-    },[])
-    const handleGetData=()=>{
-         getCarData().then(res=>{
+        handleGetData(filter)
+    },[filter])
+    const handleGetData=(filter)=>{
+         getCarData(filter).then(res=>{
             setDatas(res.data)
             setIsLoading(false)
          }).catch(err=>{console.log(err)})
+    }
+    const handleFilter=(e)=>{
+       setFilter(e.target.value)
     }
 if(isLoading)
 return <h3>...loading</h3>
@@ -28,9 +40,11 @@ return(<>
     <div><h3>Filter</h3></div>
     <div>
         <div><label>sort by car year</label>
-    <input type="radio" name="filter" id="filter"/></div>
+    <input type="radio" name="filter" id="filter" onChange={handleFilter} value="year"/></div>
+    <div>
     <label>sort by car type</label>
-    <input type="radio" name="filter" id="filter"/>
+    <input type="radio" name="filter" id="filter" onChange={handleFilter} value="type"/>
+    </div>
     </div>
 </div>
 <div>
